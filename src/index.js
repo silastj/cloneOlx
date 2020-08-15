13cf314081386316cb24266ee3d59ea17bc5d32f
aSilas <silastj@hotmail.com>
2020-08-15 19:15:13 -0300

    configuradoo redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+//REDUX
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+//REDUX
+
 import './index.css';
 import App from './App';
 
+//REDUCERS
+import Reducers from './Reducers';
+const store = createStore(Reducers);
+//REDUCERS
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
