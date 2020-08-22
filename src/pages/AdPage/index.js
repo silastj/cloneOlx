4e51eab61d8d1af99045d2792100138c9ea00efd
aSilas <silastj@hotmail.com>
2020-08-22 16:44:55 -0300

    Anuncio completo

--- src/pages/AdPage/index.js
+++ src/pages/AdPage/index.js
@@ -1,5 +1,10 @@
 import React , { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
+
+
 import {PageArea, Fake} from './styled';
 import useApi from '../../helpers/Api';
 
@@ -49,11 +54,21 @@ const Page = () => {
                 <div className="leftSide">
                     <div className="box">
                         <div className="adImage">
-                                {loading && <Fake height={300}/>}
+                                {loading && <Fake height={300}>Carregando ...</Fake>}
+                                {adInfo.images &&
+
+                                    <Slide>
+                                        {adInfo.images.map((img, k)=>
+                                            <div key={k} className="each-slide">
+                                                <img src={img} alt=""/>
+                                            </div>
+                                        )}
+                                    </Slide>
+                                }
                         </div>
                         <div className="adInfo">
                             <div className="adName">
-                                {loading && <Fake height={20}/> }
+                                {loading && <Fake height={20}>Carregando ...</Fake> }
                                 {adInfo.title &&
                                     <h2>{adInfo.title}</h2>
                                 }
@@ -62,7 +77,7 @@ const Page = () => {
                                 }
                             </div>
                             <div className="adDescription">
-                                {loading && <Fake height={90}/> }
+                                {loading && <Fake height={90}>Carregando ...</Fake> }
                                 {adInfo.description}
                                 <hr/>
                                 {adInfo.views &&
@@ -71,14 +86,61 @@ const Page = () => {
                             </div>
                         </div>
                     </div>
+{/* 
+                    <div className="rightSide twoMobile">
+                    <div className="box b-padding">
+                        {loading && <Fake height={20}>Carregando ...</Fake>}
+                        {adInfo.priceNegotiable &&
+                           "Preço Negociável"
+                        }
+                        {!adInfo.priceNegotiable && adInfo.price &&
+                            <div className="price">Preço: <span>R${adInfo.price}</span></div>
+                            
+                        }
+                        
+                    </div>
+
+                    {loading && <Fake height={20}/>}
+                    {adInfo.userInfo &&
+                        <>
+                            <a href={`mailto:${adInfo.userInfo.email}`} target="_blank" className="contactSellerLink">Fale com o vendedor</a>       
+                            <div className="box b-padding">
+                                    <h3>Criado por: <span><strong>{adInfo.userInfo.name}</strong></span></h3>
+                                    <p>E-mail:{adInfo.userInfo.email}</p>
+                            </div>
+                        </>
+                    
+                    }
+                   
+                    
+                </div> */}
+
                 </div>
-                <div className="rightSide">
+                <div className="rightSide twoDesktop">
                     <div className="box b-padding">
-                        {loading && <Fake height={20}/>}
+                        {loading && <Fake height={20}>Carregando ...</Fake>}
+                        {adInfo.priceNegotiable &&
+                           "Preço Negociável"
+                        }
+                        {!adInfo.priceNegotiable && adInfo.price &&
+                            <div className="price">Preço: <span>R${adInfo.price}</span></div>
+                        }
+                        
                     </div>
-                    <div className="box b-padding">
-                        {loading && <Fake height={20}/>}
-                    </div>
+
+                    {loading && <Fake height={20}/>}
+                    {adInfo.userInfo &&
+                        <>
+                            <a href={`mailto:${adInfo.userInfo.email}`} target="_blank" className="contactSellerLink">Fale com o vendedor</a>       
+                            <div className="box b-padding">
+                                    <p>Criado por: <span><strong>{adInfo.userInfo.name}</strong></span></p>
+                                    <p>E-mail: <strong>{adInfo.userInfo.email}</strong></p>
+                                    <p>Estado: <strong>{adInfo.stateName}</strong></p>
+                            </div>
+                        </>
+                    
+                    }
+                   
                     
                 </div>
 
--- src/pages/AdPage/styled.js
+++ src/pages/AdPage/styled.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 
+
 export const Fake = styled.div`
     background-color: #ddd;
     height:${props=>props.height || 20}px;
@@ -18,26 +19,49 @@ export const PageArea = styled.div`
     .box{
         background-color:#fff;
         border-radius:5px;
-        box-shadow:0px 4px #999;
+        box-shadow:0px 0.5px 3px #999;
         margin-bottom:20px;
     }
     .leftSide{
         flex:1;
         margin-right:10px;
+
+        .box{
+            display:flex;
+        }
         
 
         .adImage{
+            width:320px;
+            height:320px;
+            margin-right:20px;
+            border-radius:4px;
+           
+
+                .each-slide img {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                background-size: cover;
+                border-radius:4px;
+                height: 320px;
+                padding: 40px;
+                }
             
         }
         .adInfo{
-            padding:0 10px;
-
+            padding:0 20px 0 0;
+            flex:1;
             .adName{
                 margin-bottom:20px;
 
             }
             .adDescription {
 
+                h2{
+                    margin:0;
+                    margin-top:20px;
+                }
             }
         }
 
@@ -48,9 +72,31 @@ export const PageArea = styled.div`
         width:250px;
 
 
-
+        .price span{
+            font-size:21px;
+            font-weight:700;
+            letter-spacing:1px;
+        }
+        .contactSellerLink{
+            background-color:#b0a617;
+            height:30px;
+            border-radius:5px;
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            box-shadow:0px 0px 1px ##999;
+            margin-bottom:20px;
+        }
         .b-padding{
             padding:10px;
+            
+
+            p{
+                font-size:16px;
+            }
+            p strong{
+                font-size:14px;
+            }
         }
     }
 
